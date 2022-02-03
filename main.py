8a1215698b343cc61153720ca694068e4037e6df
MarkMatta47 <82917072+MarkMatta47@users.noreply.github.com>
2022-02-02 22:04:11 -0500

    final commit

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ import requests
 import json
 import imdb
 
+# open text file
 file1 = open('output.txt', 'w')
 
 
 def main():
+    # create list of Ids
     id_num_list = ['tt7462410', 'tt5491994', 'tt0081834', 'tt0096697', 'tt2100976']
+    # loop through list and write data for each show Id to output file
     for i in range(len(id_num_list)):
         print_show_data(id_num_list[i])
         file1.write('\n')
 
+    # function call for getting top 250 show data
+    get_top250_data()
+
 
 def print_show_data(id_num: str):
+    # use secret key to get show ratings data
     loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/{id_num}"
     results = requests.get(loc)
     if results.status_code != 200:
@@ -21,6 +28,7 @@ def print_show_data(id_num: str):
         return
     data = results.json()
 
+    # loop through ratings data to format output display
     for key, value in data.items():
         if key == 'ratings':
             for i in range(len(data['ratings'])):
@@ -32,17 +40,18 @@ def print_show_data(id_num: str):
             file1.write('\n')
 
 
+def get_top250_data():
+    ia = imdb.IMDb()
+    search = ia.get_top250_tv()
+
+    # loop through top 250 shows and write them to output file
+    for i in range(250):
+        for key, value in search[i].items()[0:6]:
+            file1.write(f'{key} : {value}')
+            file1.write('\n')
+        file1.write('\n')
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-# ia = imdb.IMDb()
-# search = ia.get_top250_tv()
-#
-#
-# for i in range(250):
-#     for key, value in search[i].items()[0:6]:
-#         print(key, value)
-#     print()
-
-
