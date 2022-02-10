ae48ea1b4ed4b67b4d5f8683aec3a916b1ea2557
MarkMatta47 <82917072+MarkMatta47@users.noreply.github.com>
2022-02-10 00:58:00 -0500

    added data into top 250 table

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from typing import Tuple
 
 # open text file
 file1 = open('output.txt', 'w')
+
 show_id = 0
 title = ''
 full_title = ''
@@ -15,75 +16,90 @@ crew = ''
 imdb_rating = ''
 imdb_rating_count = ''
 
+imdb_id = ''
+imdb_id_list = []
+
 
 def main():
     # create list of Ids
-    # id_num_list = ['tt7462410', 'tt5491994', 'tt0081834', 'tt0096697', 'tt2100976']
-    # loop through list and write data for each show Id to output file
+    id_num_list = ['tt7462410', 'tt5491994', 'tt0081834', 'tt0096697', 'tt2100976']
+    # # loop through list and write data for each show Id to output file
     # for i in range(len(id_num_list)):
     #     print_show_data(id_num_list[i])
     #     file1.write('\n')
+
+    for i in range(len(id_num_list)):
+        add_rating_data_to_db(id_num_list[i])
 
     # function call for getting top 250 show data
     get_top250_data()
     conn, cursor = open_db("sprint2_db.sqlite")
     setup_db(cursor)
     # make_initial_top250(cursor)
-    print_show_data(cursor)
+    add_show_data_to_db(cursor)
+
     print(type(conn))
     close_db(conn)
 
 
-# def print_show_data(id_num: str):
-#     # use secret key to get show ratings data
-#     loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/{id_num}"
-#     results = requests.get(loc)
-#     if results.status_code != 200:
-#         print("help!")
-#         return
-#     data = results.json()
-#
-#     # loop through ratings data to format output display
-#     for key, value in data.items():
-#         if key == 'ratings':
-#             for i in range(len(data['ratings'])):
-#                 for rating_key, rating_value in data['ratings'][i].items():
-#                     file1.writelines('\t' + rating_key + ' : ' + rating_value)
-#                 file1.write('\n')
-#         else:
-#             file1.writelines(f'{key} : {value}')
-#             file1.write('\n')
-
-def print_show_data(cursor: sqlite3.Cursor):
+def print_show_data(id_num: str):
     # use secret key to get show ratings data
-    loc = f"https://imdb-api.com/en/API/Top250TVs/{secrets.secret_key}"
+    loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/{id_num}"
     results = requests.get(loc)
     if results.status_code != 200:
         print("help!")
         return
     data = results.json()
 
+    # loop through ratings' data to format output display
+    for key, value in data.items():
+        if key == 'ratings':
+            for i in range(len(data['ratings'])):
+                for rating_key, rating_value in data['ratings'][i].items():
+                    file1.writelines('\t' + rating_key + ' : ' + rating_value)
+                file1.write('\n')
+        else:
+            file1.writelines(f'{key} : {value}')
+            file1.write('\n')
+
+
+def add_show_data_to_db(cursor: sqlite3.Cursor):
+    # use secret key to get show ratings data
+    loc = f"https://imdb-api.com/en/API/Top250TVs/{secrets.secret_key}/"
+    results = requests.get(loc)
+    if results.status_code != 200:
+        print("help!")
+        return
+    data = results.json()
+    count = 1
     # loop through list of dictionaries assigned to "items" key
     for entry in data["items"]:
-        # loop through current show dictionary entry
-        for key, value in entry.items():
-            if key == "rank":
-                show_id = value
-            elif key == "title":
-                title = value
-            elif key == "fullTitle":
-                full_title = value
-            elif key == "year":
-                year = value
-            elif key == "crew":
-                crew = value
-            elif key == "imDbRating":
-                imdb_rating = value
-            elif key == "imDbRatingCount":
-                imdb_rating_count = value
-        cursor.execute(f'''INSERT INTO top_250_data (show_id, title, full_title, year, crew, imdb_rating, 
-                        imdb_rating_count) VALUES (?, ?, ?, ?, ?, ?, ?)''',
-                       (int(show_id), title, full_title, year, crew, imdb_rating, imdb_rating_count))
+
+        cursor.execute(f'''INSERT INTO top_250_data (id, title, fullTitle, year, crew, imDbRating, 
+                               imDbRatingCount) VALUES (?, ?, ?, ?, ?, ?, ?)''',
+                       (count, entry['title'], entry['fullTitle'], entry['year'], entry['crew'], entry['imDbRating'],
+                        entry['imDbRatingCount']))
+        count += 1
+
+
+def add_rating_data_to_db(id_num: str):
+    rating_percent_10 = ''
+    # use secret key to get show ratings data
+    loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/{id_num}"
+    results = requests.get(loc)
+    if results.status_code != 200:
+        print("help!")
+        return
+    data = results.json()
+
+    for key, value in data.items():
+        if key == 'ratings':
+            for i in range(len(data['ratings'])):
+                for rating_key, rating_value in data['ratings'][i].items():
+                    if rating_key == 'percent':
+                        rating_percent_10 = value
+            print(rating_percent_10)
+
 
 
 def get_top250_data():
@@ -106,13 +122,13 @@ def open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
 
 def setup_db(cursor: sqlite3.Cursor):
     cursor.execute('''CREATE TABLE IF NOT EXISTS top_250_data(
-    show_id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
-    full_title TEXT NOT NULL,
+    fullTitle TEXT NOT NULL,
     year TEXT NOT NULL,
     crew TEXT NOT NULL,
-    imdb_rating TEXT NOT NULL,
-    imdb_rating_count TEXT NOT NULL
+    imDbRating TEXT NOT NULL,
+    imDbRatingCount TEXT NOT NULL
     );''')
     cursor.execute('''CREATE TABLE IF NOT EXISTS ratings_data(
     imdb_id INTEGER PRIMARY KEY,
@@ -141,9 +157,9 @@ def setup_db(cursor: sqlite3.Cursor):
     );''')
 
 
-def make_initial_top250(cursor: sqlite3.Cursor):
-    cursor.execute(f'''INSERT INTO top_250_data (show_id, title, full_title, year, crew, imdb_rating, imdb_rating_count)
-                   VALUES (1004, "the show", "the 250 show", 200.98, "10 people", 7, 2987.5)''')
+# def make_initial_top250(cursor: sqlite3.Cursor):
+#     cursor.execute(f'''INSERT INTO top_250_data (show_id, title, full_title, year, crew, imdb_rating, imdb_rating_count)
+#                    VALUES (1004, "the show", "the 250 show", 200.98, "10 people", 7, 2987.5)''')
 
 
 def close_db(connection: sqlite3.Connection):
