da2834a5bd76f25fe375655e4c466150491776dc
MarkMatta47 <82917072+MarkMatta47@users.noreply.github.com>
2022-02-10 13:24:34 -0500

    fixed format strings error

--- main.py
+++ main.py
@@ -69,11 +69,11 @@ def add_show_data_to_db(cursor: sqlite3.Cursor):
 
     # loop through list of dictionaries assigned to "items" key
     for entry in data["items"]:
-        cursor.execute(f'''INSERT INTO top_250_data (imdb_id, title, fullTitle, year, crew, imDbRating,
+        cursor.execute('''INSERT INTO top_250_data (imdb_id, title, fullTitle, year, crew, imDbRating,
                         imDbRatingCount) VALUES (?, ?, ?, ?, ?, ?, ?)''',
                        (entry['id'], entry['title'], entry['fullTitle'], entry['year'], entry['crew'],
                         entry['imDbRating'], entry['imDbRatingCount']))
-    cursor.execute(f'''INSERT INTO top_250_data (imdb_id, title, fullTitle, year, crew, imDbRating,
+    cursor.execute('''INSERT INTO top_250_data (imdb_id, title, fullTitle, year, crew, imDbRating,
                             imDbRatingCount) VALUES (?, ?, ?, ?, ?, ?, ?)''',
                    ('tt7462410', 'The Wheel of Time', 'The Wheel of Time (TV Series 2021– )', '2021',
                     'People', '0', '84387'))
@@ -88,7 +88,7 @@ def add_rating_data_to_db(cursor: sqlite3.Cursor, id_num: str):
         return
     data = results.json()
 
-    cursor.execute(f'''INSERT INTO ratings_data (imdb_id, total_rating, total_rating_votes, rating_percent_10,
+    cursor.execute('''INSERT INTO ratings_data (imdb_id, total_rating, total_rating_votes, rating_percent_10,
     rating_votes_10, rating_percent_9, rating_votes_9, rating_percent_8, rating_votes_8, rating_percent_7,
     rating_votes_7, rating_percent_6, rating_votes_6, rating_percent_5, rating_votes_5, rating_percent_4,
     rating_votes_4, rating_percent_3, rating_votes_3, rating_percent_2, rating_votes_2, rating_percent_1,
