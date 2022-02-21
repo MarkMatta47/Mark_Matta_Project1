ef4b1aa9301b6189b351d683c7dd7e7e604477c2
MarkMatta47 <82917072+MarkMatta47@users.noreply.github.com>
2022-02-21 16:42:55 -0500

    updated readme file for sprint 3

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,5 @@
+from sqlalchemy.engine import cursor
+
 import main
 
 
@@ -5,3 +7,14 @@ import main
 def test_get_top250_tv():
     data = main.get_top250_data()
     assert len(data['items']) == 250
+
+
+def test_new_table():
+    main.setup_db(cursor)
+    table_exists = cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' 
+    AND name='top_250_movies' ''')
+    assert table_exists == True
+
+
+def test_write_to_table():
+    main.get_top250_movies(cursor)
